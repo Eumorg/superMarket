2b4a88928620f2e3bef141fc2a566087c2ab3a51
Eugene <eumorg@yandex.ru>
2022-08-09 15:33:20 +0700

    Header

---
+++ web/src/components/MainPage/Main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../Header/Header";
+
+function Main() {
+  return (<div>
+    <Header/>
+    <div>
+      <div>
+        Категории
+      </div>
+      <div>
+        
+      </div>
+
+    </div>
+  </div>
+
+  )
+}
+
+export default Main
