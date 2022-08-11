2e74bc12e48dd55f5964d01be201ba5252e8c6c0
IvanNovokshanov <vanotik1@gmail.com>
2022-08-11 19:56:09 +0500

    day3finalTest

--- web/src/components/Trash/Trash.jsx
+++ web/src/components/Trash/Trash.jsx
@@ -8,34 +8,45 @@ export const Trash = () => {
 	const onClickDeleteDevice = (id) => {
 		dispatch(deleteDeviceAction(id));
 	};
+	const devicesPriceAll = () =>
+		devices.reduce((acc, el) => {
+			return (acc += +el.price);
+		}, 0);
 	return (
-		<div className={classes.trash__container}>
-			<h1>Корзина</h1>
-			<table className={classes.table__container}>
-				<tbody>
-					<tr>
-						<th>№</th>
-						<th>Устройство</th>
-						<th>Цена</th>
-					</tr>
-					{devices.map((el, inx) => (
-						<tr key={el.id}>
-							<td>{inx + 1}</td>
-							<td>{el.model}</td>
-							<td>{el.price}</td>
-							<td>
-								<button onClick={() => onClickDeleteDevice(el.id)}>
-									Удалить
-								</button>
-							</td>
+		<div>
+			<h1 className={classes.trash__title}>Корзина</h1>
+			<div className={classes.trash__container}>
+				<table className='table'>
+					<tbody>
+						<tr>
+							<th>№</th>
+							<th>Устройство</th>
+							<th>Цена</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
-			{/* <h3>Итог:{devicesPriceAll}</h3> */}
-			<button type='button' className='btn btn-success'>
-				Оформить заказ
-			</button>
+						{devices.map((el, inx) => (
+							<tr key={el.id}>
+								<td>{inx + 1}</td>
+								<td>{el.model}</td>
+								<td>{el.price}</td>
+								<td>
+									<button
+										className='btn btn-warning'
+										onClick={() => onClickDeleteDevice(el.id)}
+									>
+										Удалить
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+			<div className={classes.final__price}>
+				<h3>Итог:{devicesPriceAll()}</h3>
+				<button type='button' className='btn btn-success'>
+					Оформить заказ
+				</button>
+			</div>
 		</div>
 	);
 };
