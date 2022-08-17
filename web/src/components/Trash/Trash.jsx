ef6d9095fb70202fac28d6c4d65111b70a108f07
IvanNovokshanov <vanotik1@gmail.com>
2022-08-17 20:42:43 +0500

    ivanweek2

--- web/src/components/Trash/Trash.jsx
+++ web/src/components/Trash/Trash.jsx
@@ -1,71 +1,56 @@
-import { Modal } from "../Modal/Modal";
+import { Modal } from '../Modal/Modal';
 
-import { CartItem } from "./CartItem/CartItem";
+import { CartItem } from './CartItem/CartItem';
 
-import style from "./Trash.module.css";
+import style from './Trash.module.css';
 
 export const Trash = ({ state, stateChange }) => {
-  let storage = localStorage.getItem("cart");
+	let storage = localStorage.getItem('cart');
 
-  storage = JSON.parse(storage);
+	storage = JSON.parse(storage);
 
-  const devicesPriceAll = () =>
-    storage.reduce((acc, el) => {
-      return (acc += +el.price * el.count);
-    }, 0);
+	const devicesPriceAll = () =>
+		storage.reduce((acc, el) => {
+			return (acc += +el.price * el.count);
+		}, 0);
 
-  return (
-    <div>
-      <h1>Корзина</h1>
-      <div>
-        <div>
-          <strong>
-            <div className={style.description}>
-              <div>№</div>
-              <div>Устройство</div>
-              <div>Цена</div>
-              <div>Количество</div>
-            </div>
-          </strong>
-
-          {!storage || storage.length === 0 ? (
-            <>
-              <div>
-                <div>Нет предметов в корзине</div>
-              </div>
-            </>
-          ) : (
-            <>
-              {storage.map((el, inx) => (
-                <div key={inx + 1}>
-                  <CartItem
-                    id={el.id}
-                    model={el.model}
-                    price={el.price}
-                    state={state}
-                    stateChange={stateChange}
-                    inx={inx}
-                    count={el.count}
-                  />
-                </div>
-              ))}
-            </>
-          )}
-        </div>
-      </div>
-      <div>
-        <div>
-          <h3>
-            {" "}
-            {!storage || storage.length ? (
-              <>Итого: {devicesPriceAll()}</>
-            ) : (
-              <></>
-            )}
-          </h3>
-        </div>
-        <Modal />
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className={style.trash__title}>Корзина</h1>
+			<div className={style.trash__container}>
+				{!storage || storage.length === 0 ? (
+					<div>Нет предметов в корзине</div>
+				) : (
+					<>
+						{storage.map((el, inx) => (
+							<div key={inx + 1}>
+								<CartItem
+									id={el.id}
+									model={el.model}
+									price={el.price}
+									state={state}
+									stateChange={stateChange}
+									inx={inx}
+									count={el.count}
+								/>
+							</div>
+						))}
+					</>
+				)}
+			</div>
+			<div>
+				<div>
+					<h3 className={style.trash__title}>
+						{' '}
+						{!storage || storage.length ? (
+							<>Итого: {devicesPriceAll()}</>
+						) : (
+							<></>
+						)}
+					</h3>
+				</div>
+				<Modal />
+			</div>
+		</div>
+	);
 };
